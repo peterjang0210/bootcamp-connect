469de2d544603457e886a251f275bc315120ae57
Peter Jang <peter.jang@outlook.com>
2019-02-27 14:25:12 -0500

    get route for profiles

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ const Profile = require("../models/Profile");
 const hash = require("../hash");
 
 module.exports = function (app) {
-    app.post("/api/user/registration", function(req, res){
+    app.post("/api/users/registration", function(req, res){
         const salt = hash.generateSalt();
         const newPassword = hash.encrypt(req.body.password, salt);
         const user = {
@@ -19,7 +19,7 @@ module.exports = function (app) {
         });
     });
 
-    app.post("/api/user/profile", function(req, res){
+    app.post("/api/users/profile", function(req, res){
         Profile.create(req.body).then(function(newProfile){
             res.json(newProfile);
         }).catch(function(error){
@@ -27,7 +27,7 @@ module.exports = function (app) {
         });
     });
 
-    app.post("/api/user/session", function(req, res){
+    app.post("/api/users/session", function(req, res){
         User.findOne({username: req.body.username}).then(function(user){
             const passwordCheck = hash.encrypt(req.body.password, user.salt);
             if(user.password === passwordCheck){
@@ -38,6 +38,13 @@ module.exports = function (app) {
             }
         }).catch(function(error){
             res.json({error: error});
+        });
+    });
+
+    app.get("/api/users/profile/:accessToken", function(req, res){
+        const userID = req.params.accessToken;
+        User.findById(userID).then(function(profile){
+            res.json(profile);
         });
     });
 }
