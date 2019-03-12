dfeef7ba1d2dd4fe9d79d9f0c184d6659f714967
Peter Jang <peter.jang@outlook.com>
2019-03-11 21:20:49 -0400

    renders links properly

--- client/src/Components/AppPage/MainView/ProfileView/UserProfile.js
+++ client/src/Components/AppPage/MainView/ProfileView/UserProfile.js
@@ -11,10 +11,12 @@ class UserProfile extends React.Component {
         description: "",
         links: [],
         skills: [],
-        location: ""
+        location: "",
+        newURL: "",
+        newLinkDescription: ""
     }
 
-    componentDidMount(){
+    componentDidMount() {
         $({
             url: `/api/users/${this.props.userId}`,
             method: "GET",
@@ -49,6 +51,7 @@ class UserProfile extends React.Component {
                 lastName: this.state.lastName,
                 email: this.state.email,
                 phoneNumber: this.state.phoneNumber,
+                links: this.state.links,
                 description: this.state.description,
                 employmentStatus: this.state.employmentStatus,
                 location: this.state.location
@@ -59,43 +62,55 @@ class UserProfile extends React.Component {
         })
     }
 
+    handleAdd = (event) => {
+        event.preventDefault();
+        const newLink = { URL: this.state.newURL, linkDescription: this.state.newLinkDescription }
+        this.setState({ links: this.state.links.concat(newLink) })
+    }
+
+    handleDelete = (event) => {
+        event.preventDefault();
+        const newLinkList = this.state.links.splice(event.target.id - 1, 1);
+        this.setState({links: newLinkList});
+    }
+
     render() {
         return (
             <div className="card">
                 <div className="card-body">
                     <div className="form-group">
                         <label>First Name</label>
-                        <input className="card-title form-control" value={this.state.firstName} onChange={this.handleChange} name="firstName"/>
+                        <input className="card-title form-control" value={this.state.firstName} onChange={this.handleChange} name="firstName" />
                     </div>
                     <div className="form-group">
                         <label>Last Name</label>
-                        <input className="card-title form-control" value={this.state.lastName} onChange={this.handleChange} name="lastName"/>
+                        <input className="card-title form-control" value={this.state.lastName} onChange={this.handleChange} name="lastName" />
                     </div>
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="email" className="card-text form-control" value={this.state.email} onChange={this.handleChange} name="email"/>
+                        <input type="email" className="card-text form-control" value={this.state.email} onChange={this.handleChange} name="email" />
                     </div>
                     <div className="form-group">
                         <label>Phone Number</label>
-                        <input className="card-text form-control" value={this.state.phoneNumber} onChange={this.handleChange} name="phoneNumber"/>
+                        <input className="card-text form-control" value={this.state.phoneNumber} onChange={this.handleChange} name="phoneNumber" />
                     </div>
                     <div className="form-group">
                         <label>Bio</label>
-                        <input className="card-text form-control" value={this.state.description} onChange={this.handleChange} name="description"/>
+                        <input className="card-text form-control" value={this.state.description} onChange={this.handleChange} name="description" />
                     </div>
                     <div className="form-group">
-                        <label>Links</label>
-                        {this.state.links && (this.state.links.map((link, i) =>
-                            <div key={i}>
-                                <label>URL</label>
-                                <input className="card-text form-control" value={link.URL} onChange={this.handleChange} />
-                                <label>Description</label>
-                                <input className="card-text form-control" value={link.linkDescription} onChange={this.handleChange} />
-                            </div>))}
+                        <h6>Links</h6>
+                        {this.state.links.map((link, i) => <span key={i}><a className="btn btn-primary" href={link.URL}>{link.linkDescription}</a><button onClick={this.handleDelete} id={i}>Delete</button></span>)}
+                        <hr/>
+                        <label>URL</label>
+                        <input className="card-text form-control" onChange={this.handleChange} value={this.state.newURL} name="newURL" />
+                        <label>Link Description</label>
+                        <input className="card-text form-control" onChange={this.handleChange} value={this.state.newLinkDescription} name="newLinkDescription" />
                     </div>
+                    <button onClick={this.handleAdd}>Add</button>
                     <div className="form-group">
                         <label>Employment Status</label>
-                        <input className="card-text form-control" value={this.state.employmentStatus} onChange={this.handleChange} name="employmentStatus"/>
+                        <input className="card-text form-control" value={this.state.employmentStatus} onChange={this.handleChange} name="employmentStatus" />
                     </div>
                     {/* <div className="form-group">
                         <label>Skills</label>
@@ -109,7 +124,7 @@ class UserProfile extends React.Component {
                     </div> */}
                     <div className="form-group">
                         <label>Location</label>
-                        <input className="card-text form-control" value={this.state.location} onChange={this.handleChange} name="location"/>
+                        <input className="card-text form-control" value={this.state.location} onChange={this.handleChange} name="location" />
                     </div>
                 </div>
                 <button onClick={this.handleSubmit}>Save Changes</button>
