27891461df64185fb4c5f0a28303c356585b1ba0
Manny Cortez <emailme@mannycortez.com>
2019-03-12 12:56:56 -0400

    styling for profile-view

--- client/src/Components/AppPage/MainView/ProfileView/Profile.js
+++ client/src/Components/AppPage/MainView/ProfileView/Profile.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const Profile = (props) => (
-    <div className="card">
+    <div className="card slide-in-top">
     <button type="button" onClick={props.handleCloseProfile} className="close" aria-label="Close">
     <span aria-hidden="true">&times;</span>
     </button>
         <div className="card-body">
-            <h5 className="card-title">{props.profile.firstName + " " + props.profile.lastName}</h5>
-            <p className="card-text">Email: {props.profile.email}</p>
-            <p className="card-text">Phone Number: {props.profile.phoneNumber}</p>
-            <h6 className="card-text"> Bio</h6>
+            <h3 className="card-title">{props.profile.firstName + " " + props.profile.lastName}</h3>
+            <h5 className="card-text">Email: {props.profile.email}</h5>
+            <h5 className="card-text">Phone Number: {props.profile.phoneNumber}</h5>
+            <h5 className="card-text"> Bio:</h5>
             <p className="card-text">{props.profile.description}</p>
-            <h6 className="card-text">Links</h6>
+            <h5 className="card-text">Links:</h5>
             {props.profile.links && props.profile.links.map((link, i) => 
             <div key={i}>
                 <a className="card-text" href={link.URL}>{link.linkDescription}</a>
             </div>)}
-            <p className="card-text">Employment Status: {props.profile.employmentStatus}</p>
-            <h6 className="card-text">Skills</h6>
+            <h5 className="card-text">Employment Status: {props.profile.employmentStatus}</h5>
+            <h5 className="card-text">Skills:</h5>
             {props.profile.skills && props.profile.skills.map((skill, i) =>
             <div key={i}>
                 <p className="card-text">{skill.skillName}: {skill.skillLevel}</p>
             </div>)}
-            <p className="card-text">Location: {props.profile.location}</p>
-            {props.profile.isLooking ? <p className="card-text">Looking for Employment</p>: <p className="card-text">Not Looking for Employment</p>}
+            <h5 className="card-text">Location: {props.profile.location}</h5>
+            {props.profile.isLooking ? <h5 className="card-text">Looking for Employment</h5>: <p className="card-text">Not Looking for Employment</p>}
         </div>
     </div>
 )
