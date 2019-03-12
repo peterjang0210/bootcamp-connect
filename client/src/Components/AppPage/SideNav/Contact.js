ebddd8149e7c7b929de7e542b788fd61086913d8
Peter Jang <peter.jang@outlook.com>
2019-03-12 14:35:10 -0400

    changes default images and changes path for logo to relative

--- client/src/Components/AppPage/SideNav/Contact.js
+++ client/src/Components/AppPage/SideNav/Contact.js
@@ -5,7 +5,7 @@ const Contact = props => (
   <li className="list-group-item">
     <div onClick={(e)=>{props.handleContactClick(props.id, e)}} className="contact row w-100">
         <div className="col-12 col-sm-6 col-md-3 px-0">
-            <img src="http://demos.themes.guide/bodeo/assets/images/users/m105.jpg" alt={props.firstName + ' ' + props.lastName} className={props.isLooking ? ("img-fluid rounded-circle d-block mx-auto isLooking") : ("img-fluid rounded-circle d-block mx-auto isNotLooking") } ></img>
+            <img src={"http://oakclifffilmfestival.com/assets/placeholder-user.png"} alt={props.firstName + ' ' + props.lastName} className={props.isLooking ? ("img-fluid rounded-circle d-block mx-auto isLooking") : ("img-fluid rounded-circle d-block mx-auto isNotLooking") } ></img>
         </div>
         <div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
             <span className="name lead">{props.firstName + ' ' + props.lastName}</span>
